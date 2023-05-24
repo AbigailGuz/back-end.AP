2d8564387a28e0ecd9b31e94430d245bdbb8cf96
abigail <abyguzman@live.com>
2023-05-24 19:58:52 -0300

    cambios

---
+++ src/test/java/com/port/EAG/EagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.port.EAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
