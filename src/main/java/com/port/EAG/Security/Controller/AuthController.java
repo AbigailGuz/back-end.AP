660d95f342018e84ffa1fa7fc6847e6a81b67e52
EstelaAGuzman <abyguzman@live.com>
2023-06-01 15:54:08 -0300

    cambios

--- src/main/java/com/port/EAG/Security/Controller/AuthController.java
+++ src/main/java/com/port/EAG/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://abiguzap.web.app")
+@CrossOrigin(origins = {"http://localhost:4200", "https://abiguzap.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
