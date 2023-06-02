7d7047f8b3a49b6534e831bed2af6e852341954c
EstelaAGuzman <abyguzman@live.com>
2023-06-02 16:55:10 -0300

    ..

--- src/main/java/com/port/EAG/Controller/PersonaController.java
+++ src/main/java/com/port/EAG/Controller/PersonaController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/personas")
-@CrossOrigin(origins ={ "http://localhost:4200", "https://abiguzap.web.app"})
+@CrossOrigin(origins = "https://abiguzap.web.app")
 public class PersonaController {
     
     @Autowired
