35f844ba340cf3687e2510f5f15888f164b3d332
EstelaAGuzman <abyguzman@live.com>
2023-06-01 21:13:00 -0300

    cambios

--- src/main/java/com/port/EAG/Controller/CEducacion.java
+++ src/main/java/com/port/EAG/Controller/CEducacion.java
@@ -27,8 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
-
+@CrossOrigin(origins ={ "http://localhost:4200", "https://abiguzap.web.app"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
